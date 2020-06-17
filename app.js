0ff946b0189540577c62af5e66d0bab797017c0b
Ryan Szalaty <ryan.szalaty@gmail.com>
2020-06-17 09:57:44 +0000

    Updated mongoose, enabled mongoDB for main page.

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-const express = require("express");
-const app = express();
-const request = require("request");
-const bodyParser = require("body-parser");
+const express = require("express"),
+	app = express(),
+	request = require("request"),
+ 	bodyParser = require("body-parser"),
+	mongoose = require("mongoose");
 
-let campgrounds = [
-		{name: "High Rise", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-		{name: "Divorce Mountain", image: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-		{name: "AA Cabin", image: "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-		{name: "Witch's House", image: "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60g"},
-		{name: "Liver River", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
-	];
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+//Schema setup
+
+let campgroundSchema = new mongoose.Schema({ 
+	name: String,
+	image: String
+});
+
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+/*
+Campground.create({name: "Divorce Mountain", 
+				   image: "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}, (err, campground) => {
+				  
+	if(err) {
+		console.log("ERROR.");
+	} else {
+		console.log("Successfully added.");
+		console.log(campground);
+	};
+				  });
+*/
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs"); //allows removal of .ejs from .render
@@ -19,20 +38,35 @@ app.get("/", (req, res) => {
 });
 
 app.get("/campgrounds/new", (req, res) => {
-	res.render("form.ejs");
+	res.render("form");
 });
 
 app.get("/campgrounds", (req, res) => {
-	res.render("campgroundsPage", {campgrounds: campgrounds});
+	Campground.find({}, (err, allCampgrounds) => {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("Successfully found: " + allCampgrounds);
+			res.render("campgroundsPage", {campgrounds: allCampgrounds});
+		};
+					});
 });
-
+		
 app.post("/campgrounds", (req, res) => {
 	let campName= req.body.name; //comes from name attribute
 	let campImage= req.body.image; 
 	let newCampground = {name: campName, image: campImage};
-	campgrounds.push(newCampground);
-	res.redirect("/campgrounds");
+	Campground.create(newCampground, (err, createdCampground) => {
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		};
+		
+	});
+
 });
+
 
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
 	console.log("Server initiated.");
